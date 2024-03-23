88f93f8967f637adb42721889a322f3ff2628ab9
Franck <contact@franckcolonna.fr>
2024-03-23 01:40:56 +0100

    ✨ Add status field to User model, update postcss.config.js, and create ViewUser page

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -2,16 +2,33 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
-use App\Models\User;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
+use App\Models\User;
 use Filament\Tables;
+use Filament\Forms\Form;
+use Tables\Columns\Text;
 use Filament\Tables\Table;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\Action;
+use Filament\Forms\Components\Section;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Infolists\Components\Split;
+use Filament\Notifications\Notification;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Actions\DeleteAction;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Infolists\Components\IconEntry;
+use Filament\Infolists\Components\TextEntry;
+use App\Filament\Resources\UserResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\UserResource\RelationManagers;
+use Filament\Infolists\Components\Section as SectionComponent;
 
 class UserResource extends Resource
 {
@@ -23,21 +40,90 @@ class UserResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Section::make()
+                    ->schema([
+                        TextInput::make('name')
+                            ->minLength(2)
+                            ->maxLength(20)
+                            ->columnSpan(7)
+                            ->required(),
+                        TextInput::make('email')
+                            ->email()
+                            ->columnSpan(7)
+                            ->required(),
+                        TextInput::make('password')
+                            ->password()
+                            ->autocomplete('new-password')
+                            ->revealable()
+                            ->columnSpan(7)
+                            ->required(),
+                    ])
+                    ->columns(12)
+                    ->columnSpanFull(),
             ]);
+
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('id')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('email')
+                    ->searchable()
+                    ->sortable(),
+                IconColumn::make('status')
+                    ->boolean(),
+                TextColumn::make('created_at')
+                    ->label('Created')
+                    ->sortable(),
+                TextColumn::make('updated_at')
+                    ->label('Updated')
+                    ->sortable(),
             ])
             ->filters([
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                ViewAction::make()->iconButton(),
+                EditAction::make()->iconButton()->color('gray'),
+                ActionGroup::make([
+                    Action::make('Block user') 
+                        ->action(function (User $record){
+                            $record->status = 0;
+                            $record->save();
+                        })
+                        ->successNotification(
+                            Notification::make()
+                                ->success()
+                                ->title('Success')
+                                ->body('The user has been blocked successfully.')
+                                ->icon('heroicon-o-check-circle'),
+                        )
+                        ->requiresConfirmation()
+                        ->hidden(fn (User $record) => $record->status === 0)
+                        ->color('danger'),
+                    Action::make('Unblock user') 
+                        ->action(function (User $record){
+                            $record->status = 1;
+                            $record->save();
+                        })
+                        ->successNotification(
+                            Notification::make()
+                                ->success()
+                                ->title('Success')
+                                ->body('The user has been unblocked successfully.'),
+                        ) 
+                        ->requiresConfirmation()
+                        ->hidden(fn (User $record) => $record->status === 1)
+                        ->color('danger'),
+                    DeleteAction::make()->icon(null)
+                ])->color('gray'),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -45,7 +131,22 @@ class UserResource extends Resource
                 ]),
             ]);
     }
-
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+        ->schema([
+            SectionComponent::make([
+                TextEntry::make('id'),
+                TextEntry::make('name'),
+                IconEntry::make('status')->boolean(),
+                TextEntry::make('created_at'),
+                TextEntry::make('updated_at'),
+            ]),
+            SectionComponent::make([
+                
+            ]),
+        ]);
+    }
     public static function getRelations(): array
     {
         return [
@@ -58,6 +159,7 @@ class UserResource extends Resource
         return [
             'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
+            'view' => Pages\ViewUser::route('/{record}'),
             'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
     }
