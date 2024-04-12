86b3d7ee065727cd579ba8dd2ffaebff1b6622a2
Franck <contact@franckcolonna.fr>
2024-04-13 00:54:49 +0200

    🔥 remove useless import

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,6 @@ namespace App\Models;
 use Spatie\Permission\Models\Role;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Models\Permission;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
